ebcda9dac66f42b5ea5d15561df7888fe15d452f
vishnu <vishnutvalsan77@gmail.com>
2025-07-01 15:42:28 +0530

    first commit of the weather app

---
+++ index.js
@@ -0,0 +1,22 @@
+const YOUR_API_KEY = "175ae70654209014e390d4b2ffd3d9cd";
+
+const getWeather = async () => {
+    const cityName= document.getElementById('weather-id').value
+  try {
+    const response=await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${YOUR_API_KEY}&units=metric`
+
+    );
+    if(!response.ok){
+        throw new Error
+    }
+  const data= await response.json()
+  console.log(data)
+
+  const weather= data.main.temp_min
+  console.log(weather)
+  document.getElementById('displayWeather').innerText=weather+"°C"
+  } catch (error) {
+    console.log(error)
+  }
+};
